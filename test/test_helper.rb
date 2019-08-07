e7b5555b54e4790ab33ef8799959ec6a00dc6359
Kaida Masaki <kaida@adadevelopersacademy.org>
2019-08-07 10:40:21 -0700

    Switch to using test/*_test.rb over spec/*_spec.rb
    Also add a Gemfile

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,7 +11,7 @@ require_relative "../lib/slack_api_wrapper"
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 VCR.configure do |config|
-  config.cassette_library_dir = "specs/cassettes" # folder where casettes will be located
+  config.cassette_library_dir = "test/cassettes" # folder where casettes will be located
   config.hook_into :webmock # tie into this other tool called webmock
   config.default_cassette_options = {
     record: :new_episodes,    # record new data when we don't have it yet
